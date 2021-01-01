7aa5fb4b0219b8b32ec4567e933b353df6300115
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2021-01-01 12:59:37 +0100

    Fix: fixing file differences

--- middlewares/permission.js
+++
@@ -1,9 +0,0 @@
-const { roles } = require('../config/roles');
-
-exports.grant = (action, resource) => async (req, res, next) => {
-  const permission = await roles.can(req.user.role)[action](resource);
-  if (!permission.granted) {
-    return res.status(401).send('Permission denied');
-  }
-  return next();
-};
--- tests/routes/auths.js
+++
@@ -1,1 +0,0 @@
-// No test yet for this endpoint
