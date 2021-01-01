714a1dbb4ecf368bf014c633f688bfff1eab3681
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2021-01-01 14:40:23 +0100

    Feat: updated doc

--- documentations/doc/api_project.js
+++ documentations/doc/api_project.js
@@ -1,7 +1,7 @@
 define({
   "title": "údúnkúlú API documentation",
-  "description": "This documentation describes the API endpoints, its params, request and response",
-  "url": "https://expressjs-template.netlify.app",
+  "description": "This documentation describes the endpoints, its params, request and response of this API",
+  "url": "https://udunkulu.netlify.app",
   "name": "udunkulu",
   "version": "1.0.0",
   "sampleUrl": false,
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-01T12:44:32.963Z",
+    "time": "2021-01-01T13:39:56.669Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
