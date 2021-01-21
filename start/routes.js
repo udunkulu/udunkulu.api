6d8413ec44685dbb0331d18a39c842adced38f6f
[Nathan] <[okolonathanchinedu@gmail.com]>
2021-01-21 12:15:44 +0100

    Revert "modified the start/routes.js file adding required album details and also created the album route in the routes/albums.js folder"
    This reverts commit aac2f3ad8bdcfc5b7b53205aca7be5aef1dd6574.

--- start/routes.js
+++ start/routes.js
@@ -1,7 +1,6 @@
 // require your routes here
 const users = require('../routes/users');
 const docs = require('../routes/docs');
-const albums = require('../routes/albums');
 const teams = require('../routes/teams');
 const { BASE_PATH } = require('../config/env');
 
@@ -11,8 +10,7 @@ const { BASE_PATH } = require('../config/env');
 module.exports = (app) => {
 // routes goes here
   app.use(`${BASE_PATH}/users`, users);
-  app.use(`${BASE_PATH}/teams`, teams);
-  app.use(`${BASE_PATH}/albums`, albums);
+  app.use(`${BASE_PATH}/teams`, teams)
 
   app.use(`${BASE_PATH}/docs`, docs);
   app.use('*', docs);
