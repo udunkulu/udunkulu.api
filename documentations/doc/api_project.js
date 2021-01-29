e03333d3a993ce1110e990aa3e71e4d0879765ee
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2021-01-29 11:12:30 +0100

    Doc(albums): rebuild docs for artist-album relations

--- documentations/doc/api_project.js
+++ documentations/doc/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-01-29T09:16:55.859Z",
+    "time": "2021-01-29T10:10:58.460Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
