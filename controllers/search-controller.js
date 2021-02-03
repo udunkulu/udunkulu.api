0c34d354213db77305f4403fe6d4022a2cfc6fa3
Ezeh Livinus <ezehlivinusezeh@gmail.com>
2021-02-03 15:57:53 +0100

    Feat(search): completed search

--- controllers/search-controller.js
+++ controllers/search-controller.js
@@ -5,7 +5,7 @@ const { Artist } = require('../models/artist');
 const { User } = require('../models/user');
 
 exports.search = async (req, res) => {
-  const query = new RegExp(req.query.q, 'gi');
+  const query = new RegExp(req.query.q.trim(), 'gi');
 
   const songs = await Song
     .find({ $text: { $search: query } }).select(['_id', 'title']);
@@ -19,6 +19,13 @@ exports.search = async (req, res) => {
   const users = await User
     .find({ $text: { $search: query } })
     .select(['_id', 'firstName', 'lastName']);
+
+  if (_.isEmpty(songs) && _.isEmpty(albums) && _.isEmpty(artists) && _.isEmpty(users)) {
+    return res.status(404).send({
+      success: false,
+      message: 'we could not find what you are looking for'
+    });
+  }
 
   const results = {
     songs, albums, artists, users
